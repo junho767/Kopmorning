bb7ed326526aec5401763ff698d1a8bb680a5268
wnsgh <junho767@naver.com>
2025-05-29 15:54:54 +0900

    init backend

---
+++ backend/src/main/java/com/personal/kopmorning/KopmorningApplication.java
@@ -0,0 +1,13 @@
+package com.personal.kopmorning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KopmorningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KopmorningApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/personal/kopmorning/KopmorningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.kopmorning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KopmorningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
